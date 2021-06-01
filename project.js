929c56d5c9abaf803009b2b181a50565b1f9cf5a
Norbert Matynia <norbert.matyniaa@gmail.com>
2021-06-01 20:37:18 +0200

    v3

--- project.js
+++ project.js
@@ -2,27 +2,39 @@ const randomizer = (array) => {
   return array[Math.floor(Math.random() * array.length)];
 };
 
-const activities = [
-  "running",
-  "swimming",
-  "skiing",
-  "parachuting",
-  "playing games",
-  "driving a car",
-  "cooking",
-];
-const feelings = ["hate", "love", "like", "dislike"];
-const problems = ["depression", "health", "familly problems", "work"];
-const message = (activity, feeling, problem, number) => {
-  for (let i = 0; i < number; i++) {
-    let sentence = [
-      `I ${randomizer(feeling)} ${randomizer(activity)}`,
-      `${randomizer(activity)} helps me with my ${randomizer(problem)}`,
-      `I am ${randomizer(activities)} because It makes me smile`,
-    ];
+const sentence = {
+  activities: [
+    "running",
+    "swimming",
+    "skiing",
+    "parachuting",
+    "playing games",
+    "driving a car",
+    "cooking",
+  ],
 
-    console.log(sentence[Math.floor(Math.random() * sentence.length)]);
-  }
+  feelings: ["hate", "love", "like", "dislike"],
+
+  places: ["mountains", "Los Angeles", "Dubai", "New York", "Greece"],
+
+  //code is can be expand thanks to number variable that states how many new messages are wanted
+  creator(number) {
+    for (let i = 0; i < number; i++) {
+      let activity = randomizer(this.activities);
+      let feeling = randomizer(this.feelings);
+      let activity2 = randomizer(
+        this.activities.filter((word) => word != activity)
+      );
+      let place = randomizer(this.places);
+      let sentence = [
+        `I ${feeling} ${activity} and ${activity2}`,
+        `I dream about ${activity} in ${place}`,
+        `I would like to live in ${place}`,
+      ];
+
+      console.log(sentence[Math.floor(Math.random() * sentence.length)]);
+    }
+  },
 };
 
-message(activities, feelings, problems, 3);
+sentence.creator(3);
